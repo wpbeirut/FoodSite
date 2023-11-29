e06601aab9b18d349264fa55a1c044e6575609b3
Fadi Nicolas Zahhar <79freelancer@gmail.com>
2023-11-29 17:03:43 +0200

    adding the toggole event and menu javascript

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import _ from "lodash";
 import "./_vendor.scss";
 
+document.querySelector(".menu").addEventListener
+('click',() => {
+	document.querySelectorAll(".target").forEach((item) => {
+		item.classList.toggle('change')
+	})
+})
+
+
 const icons = document.querySelectorAll('.section-1-icons i')
 let i = 1
 
